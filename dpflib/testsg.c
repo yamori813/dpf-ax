134dff470581e80189a91195ad93b9b5e1b42d6e
Hiroki Mori <yamori813@yahoo.co.jp>
2020-01-27 16:49:59 +0900

    add dump

--- dpflib/testsg.c
+++ dpflib/testsg.c
@@ -1,14 +1,30 @@
 #include "dpf.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main()
 {
 int ret;
 DPFHANDLE h;
 unsigned char id[3];
+unsigned char *buff;
+int i, fd, size, offset;
 
 	ret = dpf_open("/dev/sga", &h);
 	flash_probe(h, id);
 	printf("%2d %2d %2d\n", id[0], id[1], id[2]);
+	size = 1024 * 4;
+	buff = (unsigned char *)malloc(size);
+	fd = open("dumpfile", O_WRONLY|O_CREAT|O_TRUNC, 0666);
+	offset = 0;
+	for (i = 0; i < 256; ++i) {
+		flash_read(h, buff, offset, size);
+		write(fd, buff, size);
+		offset += size;
+	}
+        close(fd);
+
 	dpf_close(h);
 }
